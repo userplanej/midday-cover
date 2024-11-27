7c74cd43d9f524fff1d7e868938bccdeebce53d1
Lee jaehyu <jaehyu@userplane.co.kr>
2024-11-28 08:44:19 +0900

    New chat

--- apps/dashboard/src/components/assistant/toolbar.tsx
+++ apps/dashboard/src/components/assistant/toolbar.tsx
@@ -4,15 +4,14 @@ type Props = {
 
 export function Toolbar({ onNewChat }: Props) {
   return (
-    <button onClick={onNewChat} type="button">
-      <div className="left-4 right-4 absolute z-20 bottom-4 flex items-center justify-center">
-        <div className="dark:bg-[#1A1A1A]/95 bg-[#F6F6F3]/95 h-8 w-full justify-between items-center flex px-2 space-x-4 text-[#878787]">
-          <div className="flex items-center space-x-3">
+    <button onClick={onNewChat}  type="button">
+      <div className=" flex items-center justify-center">
+        <div className="  dark:text-white  rounded-full  h-6 w-full justify-between items-center flex px-2 space-x-2 text-[#878787]">
             <kbd className="pointer-events-none h-5 select-none items-center gap-1.5 rounded border bg-accent px-1.5 font-mono text-[11px] font-medium flex bg-[#2C2C2C]">
               <span className="text-[16px]">⌘</span>J
             </kbd>
-            <span className="text-xs">New chat</span>
-          </div>
+            <span className="text-xs">새로고침</span>
+        
         </div>
       </div>
     </button>
--- apps/dashboard/src/components/chat/chat-footer.tsx
+++ apps/dashboard/src/components/chat/chat-footer.tsx
@@ -4,13 +4,15 @@ import { Popover, PopoverContent, PopoverTrigger } from "@midday/ui/popover";
 import { app, platform } from "@todesktop/client-core";
 import { isDesktopApp } from "@todesktop/client-core/platform/todesktop";
 import { useRouter } from "next/navigation";
+import { Toolbar } from "../assistant/toolbar";
 
 type Props = {
   onSubmit: () => void;
   showFeedback: () => void;
+  onNewChat: () => void;
 };
 
-export function ChatFooter({ onSubmit, showFeedback }: Props) {
+export function ChatFooter({ onSubmit, showFeedback, onNewChat }: Props) {
   const router = useRouter();
   const { setOpen } = useAssistantStore();
 
@@ -24,90 +26,7 @@ export function ChatFooter({ onSubmit, showFeedback }: Props) {
 
   return (
     <div className="hidden todesktop:flex md:flex px-3 h-[40px] w-full border-t-[1px] items-center bg-background backdrop-filter dark:border-[#2C2C2C] backdrop-blur-lg dark:bg-[#151515]/[99]">
-      <Popover>
-        <PopoverTrigger>
-          <div className="scale-50 opacity-50 -ml-2">
-            <Icons.LogoSmall />
-          </div>
-        </PopoverTrigger>
-
-        <PopoverContent
-          className="bg-background backdrop-filter dark:border-[#2C2C2C] backdrop-blur-lg dark:bg-[#1A1A1A]/95 p-2 rounded-lg -ml-2 w-auto"
-          side="top"
-          align="start"
-          sideOffset={10}
-        >
-          <ul className="flex flex-col space-y-2">
-            <li>
-              <button
-                type="button"
-                className="flex space-x-2 items-center text-xs hover:bg-[#F2F1EF] dark:hover:bg-[#2b2b2b] rounded-md transition-colors w-full p-1"
-                onClick={() => handleOpenUrl("https://x.com/middayai")}
-              >
-                <Icons.X className="w-[16px] h-[16px]" />
-                <span>Follow us</span>
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="flex space-x-2 items-center text-xs hover:bg-[#F2F1EF] dark:hover:bg-[#2b2b2b] rounded-md transition-colors w-full p-1"
-                onClick={() => handleOpenUrl("https://go.midday.ai/anPiuRx")}
-              >
-                <Icons.Discord className="w-[16px] h-[16px]" />
-                <span>Join Our Community</span>
-              </button>
-            </li>
-
-            <li>
-              <button
-                type="button"
-                className="flex space-x-2 items-center text-xs hover:bg-[#F2F1EF] dark:hover:bg-[#2b2b2b] rounded-md transition-colors w-full p-1"
-                onClick={showFeedback}
-              >
-                <Icons.ChatBubble className="w-[16px] h-[16px]" />
-                <span>Send feedback</span>
-              </button>
-            </li>
-
-            <li>
-              <button
-                type="button"
-                className="flex space-x-2 items-center text-xs hover:bg-[#F2F1EF] dark:hover:bg-[#2b2b2b] rounded-md transition-colors w-full p-1"
-                onClick={() => handleOpenUrl("https://git.new/midday")}
-              >
-                <Icons.GithubOutline className="w-[16px] h-[16px]" />
-                <span>Github</span>
-              </button>
-            </li>
-
-            <li>
-              <button
-                type="button"
-                className="flex space-x-2 items-center text-xs hover:bg-[#F2F1EF] dark:hover:bg-[#2b2b2b] rounded-md transition-colors w-full p-1"
-                onClick={() => {
-                  router.push("/account/assistant");
-                  setOpen();
-                }}
-              >
-                <Icons.Settings className="w-[16px] h-[16px]" />
-                <span>Settings</span>
-              </button>
-            </li>
-
-            <li className="hidden todesktop:block">
-              <button
-                type="button"
-                className="flex space-x-2 items-center text-xs hover:bg-[#F2F1EF] dark:hover:bg-[#2b2b2b] rounded-md transition-colors w-full p-1 text-destructive"
-                onClick={() => app.quit()}
-              >
-                <Icons.ExitToApp className="w-[16px] h-[16px]" />
-                <span>Quit Midday</span>
-              </button>
-            </li>
-          </ul>
-        </PopoverContent>
-      </Popover>
+       <Toolbar onNewChat={onNewChat} />
 
       <div className="ml-auto flex space-x-4">
         <button
